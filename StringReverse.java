c4aca8d39cf14309487296184a1aab7c98e6d309
RishuGoenka <goenkarishunew@gmail.com>
2017-05-19 18:53:13 +0530

    Created StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class StringReverse {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+			StringBuilder string = new StringBuilder();
+			System.out.println(string.append(read.readLine().toString()).reverse());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
