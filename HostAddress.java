19f8791540f2907492c8b15ca75961bbed004abe
RishuGoenka <goenkarishunew@gmail.com>
2017-05-19 18:48:42 +0530

    Created HostAddress.java

---
+++ HostAddress.java
@@ -0,0 +1,13 @@
+import java.net.*;
+
+public class HostAddress {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println(InetAddress.getLocalHost());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
