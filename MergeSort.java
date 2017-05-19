b8b2c88791dd98118e92c14ca82412efaff172f2
RishuGoenka <goenkarishunew@gmail.com>
2017-05-19 18:42:57 +0530

    Create MergeSort.java
    Logic part is left

---
+++ MergeSort.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MergeSort {
+
+	public static void main(String[] args) {
+		try {
+			BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+			int arrayLength;
+			int arr[];
+			System.out.println("Enter the array Length");
+			arrayLength = Integer.parseInt(read.readLine());
+			arr = new int[arrayLength];
+			for (int i = 0; i < arrayLength; i++)
+				arr[i] = Integer.parseInt(read.readLine());
+
+			// Logic implementation left
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
